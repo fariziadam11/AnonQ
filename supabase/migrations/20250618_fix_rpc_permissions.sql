e78488fafd7603aac234fa499b5d67d41993f77b
fariziadam11 <fariziadam508@gmail.com>
2025-06-18 21:26:39 +0700

    delete: delete migrations

--- supabase/migrations/20250618_fix_rpc_permissions.sql
+++
@@ -1,3 +0,0 @@
--- Grant permission to the anon role to execute the get_message_stats function
--- This allows guest users to view message statistics on profile pages without causing an authorization error.
-GRANT EXECUTE ON FUNCTION get_message_stats(uuid) TO anon;
